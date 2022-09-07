d4dfac518397455a0ca3b5fdee6d373dab40c8ac
kevingweinberg <kevingweinberg@gmail.com>
2022-09-06 21:27:30 -0400

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      brown: "#6F4E37",
+    },
+  },
+  plugins: [],
+};
